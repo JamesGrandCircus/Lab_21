1c07dec3a15e88312b3a6c090285b9ded055bfb7
Jacob <jacobashin@gmail.com>
2019-01-28 00:55:42 -0500

    First commit

---
+++ WebApplication3/Controllers/CoffeeShopController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class CoffeeShopController : Controller
+    {
+
+        // GET: CoffeeShop
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult UserRegistration()
+        {
+            ViewBag.Message= "Please register here";
+            return View();
+        }
+       [HttpPost]
+        public ActionResult SummaryPage(UserRegistrationModel userRegistration)
+        {
+            
+           
+            return View(userRegistration);
+        }
+    }
+}
---
+++ WebApplication3/Models/UserRegistrationModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class UserRegistrationModel
+    {
+        
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+       
+        public int Telephone { get; set; }
+       
+        public string email { get; set; }
+    }
+}
